d54b0ebb106a2dc4f2509d2ae95fdf515d59b654
weiShuai <601770050@qq.com>
2018-09-02 21:37:41 +0800

    Spring Boot集成lombok让代码更简洁

---
+++ spring-boot/src/main/java/com/weishuai/pojo/Sex.java
@@ -0,0 +1,27 @@
+package com.weishuai.pojo;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by WS on 2018/9/2.
+ */
+@Slf4j
+@Data
+public class Sex {
+
+    private Integer id;
+    private String sex;
+
+    @Override
+    public String toString() {
+        return "Sex [id=" + id + ", sex=" + sex + "]";
+    }
+
+    public static void main(String[] args) {
+        Sex sex = new Sex();
+        sex.setId(20);
+        sex.setSex("男");
+        log.info(sex.toString());
+    }
+}
